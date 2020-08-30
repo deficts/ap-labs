d776fb1b319eda6be94af93073386ed3883b8c3c
deficts <diegoar7@live.com>
2020-08-30 17:47:16 -0500

    Solving word-reverser

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-    // Place your magic here
-    return 0;
+    int i = 0;
+    char str[256] = "";
+    char ch;
+    while((ch = getchar()) != EOF){
+        if(ch == '\n'){
+            for(int j = strlen(str); j >=0 ;j--){
+                printf("%c",str[j]);
+            }
+            printf("\n");
+            memset(str, 0, sizeof str);
+            i=0;
+            continue;
+        }
+        str[i++] = ch;
+    }
+    printf("%s",str);
 }
