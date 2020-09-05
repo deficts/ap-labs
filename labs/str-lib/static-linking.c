12706434f8cc559d12b917798d861efa797c3d31
deficts <diegoar7@live.com>
2020-09-05 15:28:44 -0500

    Finished str-lib lab

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
-    return 0;
+int mystrlen(char *);
+char *mystradd(char *,char *);
+char mystrfind(char *,char *);
+int main(int argc, char* argv[]) {
+    if(argc == 4){
+        if(strcmp(argv[1], "-add") == 0){
+            int initialLen = mystrlen(argv[2]);
+            char* result = mystradd(argv[2],argv[3]);
+            int newLen = mystrlen(result);
+            printf("Initial Length: %d\n", initialLen);
+			printf("New String: %s\n", result);
+			printf("New length: %d\n", newLen);
+        }else if(strcmp(argv[1], "-find") == 0){
+            int result = mystrfind(argv[2],argv[3]);
+            if(result == -1){
+                printf("The string [%s] was not found\n",argv[3]);
+            }else{
+                printf("The string [%s] was found at [%d] index\n",argv[3],result);
+            }
+            
+        }
+    }else{
+        printf("Error, you entered an invalid set of parameters\n");
+    }
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,35 @@
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int i;
+    for(i = 0; str[i]!='\0'; i++);
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char *newStr = malloc(mystrlen(origin)+mystrlen(addition)+1);
+    strcpy(newStr,origin);
+    strcat(newStr,addition);
+    return newStr;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int originLen = mystrlen(origin);
+    int subLen = mystrlen(substr);
+    for(int i = 0; i<originLen; i++){
+        if(origin[i] == substr[0]){
+            int j, k;
+            for(j = i, k = 0; j<originLen && k<subLen; j++,k++){
+                if(origin[j] != substr[k]){
+                    break;
+                }
+            }
+            if(k==subLen){
+                return i;
+            }
+        }
+    }
+    return -1;
 }
