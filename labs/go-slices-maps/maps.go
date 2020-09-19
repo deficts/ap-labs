70506d12cc6c8a16ea05bce50c452e66a6028381
deficts <diegoar7@live.com>
2020-09-18 20:31:37 -0500

    solving go-slices-maps

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
-func main() {
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Split(s," ")
+	countMap := make(map[string]int)
+	for i:= 0; i < len(words); i++{
+		countMap[words[i]]+=1
+	}
+	return countMap;
 }
+
+func main() {
+	wc.Test(WordCount)
+}
+
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -3,7 +3,14 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
-     return [][]uint8{}
+    myPic := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		myPic[i] = make([]uint8, dx)
+		for j := 0; j < dx; j++ {
+			myPic[i][j] = uint8(i^j)
+		}
+	}
+	return myPic
 }
 
 func main() {
