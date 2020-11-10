7f1e05dd65209a4c01c3273047490cb0cadf8400
deficts <diegoar7@live.com>
2020-11-09 21:32:57 -0600

    Solving goroutines vs os threads

--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+var maxStages int
+
 func main() {
-	var maxStages, transitTime int
+	in := make(chan int)
+	out := make(chan int)
+
+	go connect(in, out, 0, 1000000)
+	start := time.Now()
+	in <- 1
 
 	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+	fmt.Println("Time to transit trough the pipeline : ", time.Since(start))
+}
+
+func connect(in <-chan int, out chan int, index int, connections int) {
+	if index != connections {
+		maxStages++
+		connect(in, out, index+1, connections)
+		out <- <-in
+	}
 }
